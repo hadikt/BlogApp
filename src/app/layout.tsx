9c12d960d3718ced4cb5c0a69c22981c4840fc21
HadiPlusIT <mohammedhadiplus@gmail.com>
2024-11-29 00:16:48 +0530

    dark theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,8 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import Header from "./components/Header";
-
+import {ThemeProvider} from 'next-themes'
+import Themecom from "./components/themecomponent";
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
@@ -25,12 +26,16 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressContentEditableWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <ThemeProvider>
+          <Themecom>
         <Header/>
         {children}
+        </Themecom>
+        </ThemeProvider>
       </body>
     </html>
   );
